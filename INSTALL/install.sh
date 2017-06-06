6e5ddae521ea864759062818c3eda293acca86a7
root <root@localhost.localdomain>
2017-06-06 15:28:33 +0800

    install.sh：安装时要自动运行app.py

--- INSTALL/install.sh
+++ INSTALL/install.sh
@@ -3,3 +3,6 @@ cp -rf opt etc /
 
 # 注意：由于是使用root运行的，所以一定要获得实际登录用户
 sudo -u $LOGNAME xdg-desktop-icon install HOME/*
+
+
+`grep Exec etc/xdg/autostart/app-autostart.desktop | cut -d= -f2` &>/dev/null &

76f2fe5052bb9fdf172360767f9464bd2e6481ae
Jin Guojie <jinguojie@loongson.cn>
2017-06-07 18:10:58 +0800

    “我的”页面：适应/opt/app/db文件格式改成“ID：版本号:状态：日期”

--- php/getMyAppHtml.php
+++ php/getMyAppHtml.php
@@ -7,12 +7,12 @@ include_once('_rank.inc');
 include_once('_my.inc');
 
 /* 前端提交的$app_list_data：
- *   系统目录下，每一个已安装应用的文件名：“ID：版本号”，回车符分割
+ *   系统目录下，每一个已安装应用的文件名：“ID：版本号:状态：日期”，回车符分割
  *
  * 例如：
  *
- *  1:1.0.015
- *  2:2.3.245.2500
+ *  1:1.0.015:状态:日期
+ *  2:2.3.245.2500:状态:日期
  */
 $app_list_data = $_POST['data'];
 
@@ -24,12 +24,16 @@ foreach ($ss as $a)
   $p = explode(":", $a);
   $app_id = $p[0];
   $version = $p[1];
+  $status = $p[2];
 
-  $html = $html . get_app_card_html($app_id);
+  if ($status == "installed")
+  {
+    $html = $html . get_app_card_html($app_id);
 
-  /* 如果有升级，则需要显示一个额外的红点 */
-  if (app_version_compare($version, get_app_version($app_id)) < 0)
-    $html = $html . get_app_update_icon_html($app_id);
+    /* 如果有升级，则需要显示一个额外的红点 */
+    if (app_version_compare($version, get_app_version($app_id)) < 0)
+      $html = $html . get_app_update_icon_html($app_id);
+  }
 }
 
 echo $html;

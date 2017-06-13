4f92125cfb61e98a05ef0352dc07b29b71e2039f
Jin Guojie <jinguojie@loongson.cn>
2017-06-13 14:56:01 +0800

    (#19) 支持wps-office实际文件（171MB）下载
    现在一个文件最大512MB。

--- php/getAppFile.php
+++ php/getAppFile.php
@@ -8,9 +8,13 @@ $HOT_DIR = "../data/app/";
 
 $id = $_GET['id'];
 $file = $HOT_DIR . get_app_file_by_id($id);
+
+set_time_limit(0);
+ini_set('memory_limit', '512M');
 header("Content-type: octet/stream");
 header("Content-disposition:attachment;filename=".basename($file));
 header("Content-Length:".filesize($file));
+ob_end_clean();
 readfile($file);
 exit;
 ?>

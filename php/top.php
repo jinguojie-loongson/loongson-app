715a3d3d5b24514b6fe4c4d779a0db4914f0b0cc
Jin Guojie <jinguojie@loongson.cn>
2017-06-22 13:28:14 +0800

    (#29) 页面顶部导航：加小图标
    同时去除原来的下划线

--- php/top.php
+++ php/top.php
@@ -3,10 +3,10 @@
 </div>
 
 <div class="topbar">
-  <div class="topbar-button" id="top-home">首页</div>
-  <div class="topbar-button" id="top-rank">榜单</div>
-  <div class="topbar-button" id="top-category">分类</div>
-  <div class="topbar-button" id="top-my">我的<span class="update-circle">0</span></div>
+  <div class="topbar-button" id="top-home"><i class="fa fa-home"></i> 首页</div>
+  <div class="topbar-button" id="top-rank"><i class="fa fa-flag"></i> 榜单</div>
+  <div class="topbar-button" id="top-category"><i class="fa fa-delicious"></i> 分类</div>
+  <div class="topbar-button" id="top-my"><i class="fa fa-user-circle"></i> 我的<span class="update-circle">0</span></div>
 
   <!-- 隐藏一个刷新按钮 --!>
   <input type="button" class="invisible-button" onclick="javascript: window.location.reload();" value="">

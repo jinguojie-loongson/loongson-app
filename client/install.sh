7a5a4c7a2f6ec380f2826dbee46f407d32f7efa2
Jin Guojie <jinguojie@loongson.cn>
2017-06-28 10:06:25 +0800

    (#42) 客户端：安装程序对root用户出错

--- client/install.sh
+++ client/install.sh
@@ -2,9 +2,21 @@
 cp -rf opt /
 
 # 注意：由于是使用root运行的，所以一定要获得实际登录用户
-DEST=/home/$LOGNAME/桌面
-cp HOME/*  $DEST
-chown $LOGNAME:$LOGNAME $DEST/*
+ALL_HOME=`cut -d: -f6 /etc/passwd | grep -E   'root|home' | sort | uniq`
+
+for home in $ALL_HOME; do
+  USER=`basename $home`
+
+  for dir in "桌面"  "Desktop" ; do
+    if [ -d "$home/$dir/" ]; then
+      cp HOME/*  $home/$dir/
+      chown $USER:$USER $home/$dir/*
+    fi
+  done
+done
+
+cp HOME/*  /etc/skel/桌面/
+cp HOME/*  /etc/skel/Desktop/
 
 # 自动运行后台服务
 cp -rf etc/init.d/* /etc/init.d
--- client/opt/app/uninstall.sh
+++ client/opt/app/uninstall.sh
@@ -7,7 +7,20 @@ rm -f /etc/rc3.d/*loongson-app*
 rm -f /etc/rc5.d/*loongson-app*
 
 # 注意：由于是使用root运行的，所以一定要获得实际登录用户
-rm -f /home/$LOGNAME/桌面/loongson-app.desktop
+ALL_HOME=`cut -d: -f6 /etc/passwd | grep -E   'root|home' | sort | uniq`
+
+for home in $ALL_HOME; do
+  USER=`basename $home`
+
+  for dir in "桌面"  "Desktop" ; do
+    if [ -d "$home/$dir/" ]; then
+      rm -f HOME/*  $home/$dir/loongson-app.desktop
+    fi
+  done
+done
+
+rm -f  /etc/skel/Desktop/loongson-app.desktop
+rm -f  /etc/skel/桌面/loongson-app.desktop
 
 # 删除安装文件
 cd /opt/app

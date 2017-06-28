f7acc0d9824014ed1a7117bde77e3b5b2224be56
Jin Guojie <jinguojie@loongson.cn>
2017-06-28 11:06:02 +0800

    (#42) 客户端：中标32位root用户调用Chrome要加参数

--- client/opt/app/find-chrome.sh
+++ client/opt/app/find-chrome.sh
@@ -2,8 +2,13 @@
 
 CMD=chromium-browser
 
+#中标32位桌面
 if [ -f /usr/bin/chrome.sh ]; then
   CMD="/opt/chrome31/chrome --no-sandbox"
+
+  if [ `whoami` == "root" ]; then
+    CMD="$CMD --user-data-dir=/tmp"
+  fi
 fi
 
 $CMD $*

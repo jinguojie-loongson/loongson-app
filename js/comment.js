047d2a31496b66cb506f2d9497aed6c9d5a71c72
Jiang Xinshang <jiangxinshang@loongson.cn>
2017-08-01 09:28:49 +0800

    (#25) 评论功能：Ajax的comment参数要做URL转义 使用Ajax进行参数的传递 解决参数传递时特殊字符丢失问题

--- js/comment.js
+++ js/comment.js
@@ -7,29 +7,36 @@ function reset_comment_input()
 function submit_comment()
 {
 	var id = $("#app_id").val();
-       	var comment = $(".app-comment-input").text();
+	var comment = $(".app-comment-input").text();
 	var len = comment.length;
+	var comment_json={
+          "comment_json":{
+                         "id" : id,
+                         "comment":comment
+                         }
+        };
+	var obj=JSON.stringify(comment_json); 
 
 	if (len == 0) {
-               	warning_message("评论内容不能为空！");
-       	} else if (len > 500) {
-               	warning_message("评论内容不能超过500字！");
-       	} else {
-              	url = window.location.href;
-               	n = url.lastIndexOf("/");
-               	url = url.substr(0, n) + "/addAppComment.php?" + "id=" + id + "&comment=" + encodeURI(comment);
+		warning_message("评论内容不能为空！");
+	} else if (len > 500) {
+		warning_message("评论内容不能超过500字！");
+	} else {
+		url = window.location.href;
+		n = url.lastIndexOf("/");
+		url = url.substr(0, n) + "/addAppComment.php?";
 
-               	console.log(url);
-               	/*
-               	 * 提交评论，返回提交内容html
-               	 */
-               	get_server_service(url, "", function(data) {
-               	        $("#prompt").hide();
-			$(".app-comment-list").prepend(data);
-               	        reset_comment_input();
-		        success_message("评论提交成功！");
-               	});
-       	}
+		console.log(url);
+		/*
+		* 提交评论，返回提交内容html
+		*/
+		get_server_service_json(url, obj, function(data) {
+			$("#prompt").hide();
+			$(".app-comment-list").prepend(data.return_html);
+			reset_comment_input();
+			success_message("评论提交成功！");
+		});
+	}
 }
 
 $(document).ready(function(){
--- php/addAppComment.php
+++ php/addAppComment.php
@@ -40,14 +40,18 @@
 include_once('_util.inc');
 include_once('_comment.inc');
 
-$id = is_empty($_GET['id']) ? $_POST['data'] : $_GET['id'];
-$comment = is_empty($_GET['comment']) ? $_POST['data'] : $_GET['comment'];
+$J=json_decode($_POST['data']); 
+$id=$J -> comment_json -> id;
+$comment=$J -> comment_json -> comment;
 
 insert_app_comment($id, $comment);
 
 // 返回一个HTML，前端更新。
 $comment_id = get_app_newest_comment($id);
-echo get_app_one_comment_html($comment_id);
+
+$return_html = array('return_html'=>get_app_one_comment_html($comment_id));
+
+echo json_encode($return_html); 
 
 exit;
 ?>

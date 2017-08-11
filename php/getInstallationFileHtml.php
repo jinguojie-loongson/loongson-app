d5f0460f168b16008d53d9b032845136459a6292
Ren Yafei <renyafei@loongson.cn>
2017-08-11 09:33:08 +0800

    (#64)应用审核页面：把安装文件相关的列合并起来
    get_installationFile_by_appId_version_osId方法名改为：
    get_installationFileHTML_by_appId_version_osId

--- php/getInstallationFileHtml.php
+++ php/getInstallationFileHtml.php
@@ -3,6 +3,6 @@ include_once('_vendor.inc');
  $appid = $_POST['appid'];
  $version = $_POST['version'];
  $os_id = $_POST['os_id'];
- $result = get_installationFile_by_appId_version_osId($appid,$version,$os_id);
+ $result = get_installationFileHTML_by_appId_version_osId($appid,$version,$os_id);
 echo $result;
 ?>

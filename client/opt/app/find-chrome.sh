ed04e5c5df1d773b4bbcb9a40238f074a987ff5e
Jin Guojie <jinguojie@loongson.cn>
2017-08-25 09:09:36 +0800

    (#71) 普华操作系统：客户端适配
    /opt/chrome39/chrome -app=http://xxx

--- client/opt/app/find-chrome.sh
+++ client/opt/app/find-chrome.sh
@@ -2,6 +2,11 @@
 
 CMD=chromium-browser
 
+#普华桌面5.0
+if [ -f /opt/chrome39/chrome ]; then
+  CMD="/opt/chrome39/chrome"
+fi
+
 #中标32位桌面
 if [ -f /usr/bin/chrome.sh ]; then
   CMD="/opt/chrome31/chrome --no-sandbox"

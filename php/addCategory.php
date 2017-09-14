80b1a6435427d1a56762c622f4daea62e3740767
Ren Yafei <renyafei@loongson.cn>
2017-09-14 10:21:53 +0800

    (#99) 在服务器192.168.200.22 上的php版本是5.3：评论内容转义用的函数，转义错误 使用htmlspecialchars替换htmlentities函数

--- php/addCategory.php
+++ php/addCategory.php
@@ -5,6 +5,6 @@ include_once('_category.inc');
 $J=json_decode($_POST['data']);
 $category_name=addslashes($J -> category_json -> category_name);
 
-$result = htmlentities(add_category($category_name));
+$result = htmlspecialchars(add_category($category_name));
 echo $result;
 ?>
